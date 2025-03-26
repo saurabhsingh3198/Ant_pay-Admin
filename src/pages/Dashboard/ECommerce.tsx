de9af3d0fa538c63987dc8e4cb7dd4ed29fd88d3
saurabhsingh3198 <saurabh.singh@antworksmoney.com>
2025-03-26 14:22:43 +0530

    fix 26/03/2025

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -99,7 +99,6 @@ const ECommerce: React.FC = () => {
           </svg>
         </CardDataStats>
       </div>
-      <FolderDropdown folders={folders} loading={false} />
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
