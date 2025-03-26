e048bfa30b3ba8da3bf8571cfbc00e731e8c4d10
saurabhsingh3198 <saurabh.singh@antworksmoney.com>
2025-03-26 12:52:59 +0530

    fix 26/03/2025

--- src/components/Header/DropdownUser.tsx
+++ src/components/Header/DropdownUser.tsx
@@ -22,9 +22,9 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+            Saurabh Singh
           </span>
-          <span className="block text-xs">UX Designer</span>
+          <span className="block text-xs">Admin</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
