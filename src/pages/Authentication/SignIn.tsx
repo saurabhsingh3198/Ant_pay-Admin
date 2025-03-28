0b0b1f1c4182a6899069a0aad29425e934214def
saurabhsingh3198 <saurabh.singh@antworksmoney.com>
2025-03-28 17:23:23 +0530

    fix 28/03/2025

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -256,7 +256,7 @@ const SignIn: React.FC = () => {
                       </label>
                       <div className="relative">
                         <input
-                          type="email"
+                          type="number"
                           placeholder="Enter your Mobile Number"
                           value={fieldData.email}
                           className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
