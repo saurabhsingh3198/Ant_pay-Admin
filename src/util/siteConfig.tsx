c565e09e77b67d934c61780e92c9b6222ae51a25
saurabh <saurabh.dubey@antworksmoney.com>
2025-04-07 12:25:14 +0530

    login url in config

--- src/util/siteConfig.tsx
+++ src/util/siteConfig.tsx
@@ -3,4 +3,17 @@ const siteCofig = {
     USER_LOGIN: "",
 };
 
+// User
+// Login - https://affiliate.antworksmoney.in/api/auth/user/send-otp
+// Body - phone
+// Verify - https://affiliate.antworksmoney.in/api/auth/user/verify-otp
+// Body - "phone" : "xxxxxxxxx", "otp"    : "xxxxxx"
+
+// Admin 
+// Login - https://affiliate.antworksmoney.in/api/auth/admin/send-otp
+// Body - phone
+// Verify - https://affiliate.antworksmoney.in/api/auth/admin/verify-otp
+// Body - "phone" : "xxxxxxxxx", "otp"    : "xxxxxx"
+
+
 export default siteCofig;
