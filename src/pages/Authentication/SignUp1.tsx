28b805918858d98a9438c15b0fde8d6acde4ead1
saurabh <saurabh.dubey@antworksmoney.com>
2025-04-09 18:19:50 +0530

    logo add in signup page

--- src/pages/Authentication/SignUp1.tsx
+++ src/pages/Authentication/SignUp1.tsx
@@ -101,9 +101,16 @@ export default function SignUp1() {
       <div className="flex flex-wrap items-center">
         <div className="w-full border-stroke dark:border-strokedark">
           <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-            <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-              Sign Up to Portal
-            </h2>
+            <div className="flex justify-center mb-2">
+              <img
+                className="dark:block"
+                src={'https://www.antworksmoney.com/assets/img/logo3.png'}
+                alt="Antworks Money Logo"
+              />
+              {/* <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
+                Sign Up to Portal
+              </h2> */}
+            </div>
 
             <Formik
               initialValues={initialValues}
