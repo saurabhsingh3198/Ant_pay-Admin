6043c5dcc7a5aab3733b5834ad14b06eac93e481
saurabh <saurabh.dubey@antworksmoney.com>
2025-04-09 10:57:37 +0530

    modification done in signup form

--- src/pages/Authentication/SignUp1.tsx
+++ src/pages/Authentication/SignUp1.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SelectGroupTwo from '../../components/Forms/SelectGroup/SelectGroupTwo';
 import { FaAddressCard, FaBirthdayCake, FaIdCard } from 'react-icons/fa';
-import { LiaFingerprintSolid } from 'react-icons/lia';
+import { LiaCitySolid, LiaFingerprintSolid } from 'react-icons/lia';
 import siteConfig from '../../util/siteConfig';
 import axios from 'axios';
 import SelectGroupOne from '../../components/Forms/SelectGroup/SelectGroupOne';
@@ -435,6 +435,153 @@ const SignUp1: React.FC = () => {
 
                       <span className="absolute right-4 top-4">
                         <FaAddressCard size={22} />
+                      </span>
+                    </div>
+                  </div>
+                  <div className="mb-4 w-1/2">
+                    <label className="mb-2.5 block font-medium text-black dark:text-white">
+                      State
+                    </label>
+                    <div className="relative z-20 bg-transparent dark:bg-form-input">
+                      <select
+                        value={selectedOption}
+                        onChange={(e) => {
+                          setSelectedOption(e.target.value);
+                          changeTextColor();
+                        }}
+                        className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${
+                          isOptionSelected ? 'text-black dark:text-white' : ''
+                        }`}
+                      >
+                        <option
+                          value=""
+                          disabled
+                          className="text-body dark:text-bodydark"
+                        >
+                          Select State
+                        </option>
+                        <option
+                          value="State 1"
+                          className="text-body dark:text-bodydark"
+                        >
+                          State 1
+                        </option>
+                        <option
+                          value="State 2"
+                          className="text-body dark:text-bodydark"
+                        >
+                          State 2
+                        </option>
+                      </select>
+
+                      <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                        <svg
+                          className="fill-current"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g opacity="0.8">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                              fill=""
+                            ></path>
+                          </g>
+                        </svg>
+                      </span>
+                    </div>
+
+                      <span className="absolute right-4 top-4">
+                        <FaAddressCard size={22} />
+                      </span>
+                    </div>
+                </div>
+                <div className="flex space-x-6">
+                <div className="mb-4 w-1/2">
+                    <label className="mb-2.5 block font-medium text-black dark:text-white">
+                      City
+                    </label>
+                    <div className="relative z-20 bg-transparent dark:bg-form-input">
+                      <select
+                        value={selectedOption}
+                        onChange={(e) => {
+                          setSelectedOption(e.target.value);
+                          changeTextColor();
+                        }}
+                        className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${
+                          isOptionSelected ? 'text-black dark:text-white' : ''
+                        }`}
+                      >
+                        <option
+                          value=""
+                          disabled
+                          className="text-body dark:text-bodydark"
+                        >
+                          Select City
+                        </option>
+                        <option
+                          value="City 1"
+                          className="text-body dark:text-bodydark"
+                        >
+                          City 1
+                        </option>
+                        <option
+                          value="City 2"
+                          className="text-body dark:text-bodydark"
+                        >
+                          City 2
+                        </option>
+                      </select>
+
+                      <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                        <svg
+                          className="fill-current"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <g opacity="0.8">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                              fill=""
+                            ></path>
+                          </g>
+                        </svg>
+                      </span>
+                    </div>
+
+                      <span className="absolute right-4 top-4">
+                        <FaAddressCard size={22} />
+                      </span>
+                    </div>
+                  <div className="mb-4 w-1/2">
+                    <label className="mb-2.5 block font-medium text-black dark:text-white">
+                      Pin Code
+                    </label>
+                    <div className="relative">
+                      <input
+                        type="text"
+                        placeholder="Enter your Pin Code"
+                        value={fieldData.address_line}
+                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        onChange={(e) =>
+                          setFieldData((prev: any) => ({
+                            ...prev,
+                            address_line: e.target.value,
+                          }))
+                        }
+                      />
+
+                      <span className="absolute right-4 top-4">
+                        <LiaCitySolid size={22} />
                       </span>
                     </div>
                   </div>
